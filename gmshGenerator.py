85a50ea25c2a7c4edbcf588c9850b9ca11f4623b
Ship-Vano <vanotheargonianin@gmail.com>
2025-02-11 21:04:07 +0300

    fxs

--- gmshGenerator.py
+++ gmshGenerator.py
@@ -4,28 +4,32 @@ import numpy as np
 gmsh.initialize()
 
 # Set global mesh size (optional)
-lc = 3e-3
-gmsh.option.setNumber("Mesh.CharacteristicLengthMin", lc)
-gmsh.option.setNumber("Mesh.CharacteristicLengthMax", lc)
+
+
 
 Brio = True
-
+lc = 3e-3
 # Create geometry
 if Brio:
+    lc = 6e-3
     point1 = gmsh.model.geo.add_point(0.0, 0.0, 0.0, lc)
     point2 = gmsh.model.geo.add_point(1.0, 0.0, 0.0, lc)
     point3 = gmsh.model.geo.add_point(1.0, 0.1, 0.0, lc)
     point4 = gmsh.model.geo.add_point(0.0, 0.1, 0.0, lc)
 else:
-    point1 = gmsh.model.geo.add_point(0.0, 0.0, 0.0, lc)
-    point2 = gmsh.model.geo.add_point(3.0, 0.0, 0.0, lc)
+    lc = 1e-1
+    point1 = gmsh.model.geo.add_point(-3.0, -3.0, 0.0, lc)
+    point2 = gmsh.model.geo.add_point(3.0, -3.0, 0.0, lc)
     point3 = gmsh.model.geo.add_point(3.0, 3.0, 0.0, lc)
-    point4 = gmsh.model.geo.add_point(0.0, 3.0, 0.0, lc)
+    point4 = gmsh.model.geo.add_point(-3.0, 3.0, 0.0, lc)
 
 line1 = gmsh.model.geo.add_line(point1, point2)
 line2 = gmsh.model.geo.add_line(point2, point3)
 line3 = gmsh.model.geo.add_line(point3, point4)
 line4 = gmsh.model.geo.add_line(point4, point1)
+
+gmsh.option.setNumber("Mesh.CharacteristicLengthMin", lc)
+gmsh.option.setNumber("Mesh.CharacteristicLengthMax", lc)
 
 face1 = gmsh.model.geo.add_curve_loop([line1, line2, line3, line4])
 gmsh.model.geo.add_plane_surface([face1])
